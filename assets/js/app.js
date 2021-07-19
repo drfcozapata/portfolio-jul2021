3e5c21fbf74fe5818e219964b53726f58ed2e128
Francisco Zapata <drfcozapata@gmail.com>
2021-07-19 15:36:17 -0400

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+/*****
+ * AGREGA 'USE STRICT' Y VERIFICA QUE EL DOM ESTÁ CARGADO ANTES DE INICIAR EL SCRIPT
+ * @author: Francisco Zapata
+ *****/
+
+(() => {
+	'use strict';
+
+	document.addEventListener('DOMContentLoaded', () => {
+		// ESCRIBIR EL CÓDIGO A PARTIR DE AQUÍ
+	}); // DOM CONTENT LOADED
+})();
